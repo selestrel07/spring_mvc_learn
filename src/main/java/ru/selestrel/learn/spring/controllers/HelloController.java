eb46a15180b9dc8248ba236107626d8a2d2fbe69
selestrel07 <selestrel07@gmail.com>
2023-11-22 17:58:57 +0400

    explore @Controller, @GetMapping and @RequestMapping annotations

--- src/main/java/ru/selestrel/learn/spring/controllers/HelloController.java
+++ src/main/java/ru/selestrel/learn/spring/controllers/HelloController.java
@@ -1,4 +1,4 @@
-package ru.selestrel.learn.spring;
+package ru.selestrel.learn.spring.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
