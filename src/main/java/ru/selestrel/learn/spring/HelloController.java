8caf7960c21a4362c4e660724bbfdae683cfa431
selestrel07 <selestrel07@gmail.com>
2023-11-22 16:36:16 +0400

    add hello world controller + view

---
+++ src/main/java/ru/selestrel/learn/spring/HelloController.java
@@ -0,0 +1,13 @@
+package ru.selestrel.learn.spring;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HelloController {
+
+    @GetMapping("/hello-world")
+    public String sayHello() {
+        return "Hello World!";
+    }
+}
