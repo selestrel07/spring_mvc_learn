2e7f087019fc9c16f8c39625f7ff2227f7c8b60e
selestrel07 <selestrel07@gmail.com>
2023-11-28 11:34:40 +0400

    use RequestParam annotation to work with query parameters

--- src/main/java/ru/selestrel/learn/spring/controllers/FirstController.java
+++ src/main/java/ru/selestrel/learn/spring/controllers/FirstController.java
@@ -1,19 +1,17 @@
 package ru.selestrel.learn.spring.controllers;
 
-import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @RequestMapping("/first")
 public class FirstController {
 
     @GetMapping("/hello")
-    public String helloPage(HttpServletRequest request) {
-        String name = request.getParameter("name");
-        String lastName = request.getParameter("surname");
-
+    public String helloPage(@RequestParam(value = "name", required = false) String name,
+                            @RequestParam(value = "surname", required = false) String lastName) {
         System.out.println("Hello, " + name + " " + lastName);
 
         return "first/hello";
