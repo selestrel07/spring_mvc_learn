a12e7acd3290ebe3a2697eb8bbf87f35c927c191
selestrel07 <selestrel07@gmail.com>
2023-11-28 11:26:53 +0400

    use HttpServletRequest to wotk with query parameters

--- src/main/java/ru/selestrel/learn/spring/controllers/FirstController.java
+++ src/main/java/ru/selestrel/learn/spring/controllers/FirstController.java
@@ -1,5 +1,6 @@
 package ru.selestrel.learn.spring.controllers;
 
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,7 +10,12 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class FirstController {
 
     @GetMapping("/hello")
-    public String helloPage() {
+    public String helloPage(HttpServletRequest request) {
+        String name = request.getParameter("name");
+        String lastName = request.getParameter("surname");
+
+        System.out.println("Hello, " + name + " " + lastName);
+
         return "first/hello";
     }
 
