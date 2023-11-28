06224676ec0209f8dadf76a9ec66f69fd470e8ac
selestrel07 <selestrel07@gmail.com>
2023-11-28 12:10:50 +0400

    create view with calculator based on query parameters

--- src/main/java/ru/selestrel/learn/spring/controllers/FirstController.java
+++ src/main/java/ru/selestrel/learn/spring/controllers/FirstController.java
@@ -1,6 +1,7 @@
 package ru.selestrel.learn.spring.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -11,8 +12,9 @@ public class FirstController {
 
     @GetMapping("/hello")
     public String helloPage(@RequestParam(value = "name", required = false) String name,
-                            @RequestParam(value = "surname", required = false) String lastName) {
-        System.out.println("Hello, " + name + " " + lastName);
+                            @RequestParam(value = "surname", required = false) String lastName,
+                            Model model) {
+        model.addAttribute("message", "Hello, " + name + " " + lastName);
 
         return "first/hello";
     }
@@ -20,5 +22,25 @@ public class FirstController {
     @GetMapping("/goodbye")
     public String goodByePage() {
         return "first/goodbye";
+    }
+
+    @GetMapping("/calculator")
+    public String calculatorPage(@RequestParam(value = "firstNumber") int firstNumber,
+                                 @RequestParam(value = "secondNumber") int secondNumber,
+                                 @RequestParam(value = "action") String action,
+                                 Model model) {
+        String description = "Perform %s of %d and %d:".formatted(action, firstNumber, secondNumber);
+        String result = switch (action) {
+            case "multiplication" -> String.valueOf(firstNumber * secondNumber);
+            case "addition" -> String.valueOf(firstNumber + secondNumber);
+            case "subtraction" -> String.valueOf(firstNumber - secondNumber);
+            case "division" -> String.valueOf((double) firstNumber / secondNumber);
+            default -> "unknown operation, please use multiplication, addition, subtraction or division";
+        };
+
+        model.addAttribute("description", description);
+        model.addAttribute("result", result);
+
+        return "first/calculator";
     }
 }
